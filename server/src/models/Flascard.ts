0162077994ae94da8b384d89ab02226d46a7916a
SAIBEL Artem <saibel@email.cz>
2023-10-05 17:18:26 +0200

    check git contributions

--- server/src/models/Flascard.ts
+++ server/src/models/Flascard.ts
@@ -1,18 +1,18 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IFlashcard extends Document {
-    word: string;
-    definition: string;
-    category: string;
+  word: string;
+  definition: string;
+  category: string;
 }
 
 const FlashcardSchema: Schema = new Schema({
-    word: { type: String, required: true },
-    definition: { type: String, required: true },
-    category: { type: String, required: true },
-    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  word: { type: String, required: true },
+  definition: { type: String, required: true },
+  category: { type: String, required: true },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
-const Flashcard = mongoose.model<IFlashcard>('Flashcard', FlashcardSchema);
+const Flashcard = mongoose.model<IFlashcard>("Flashcard", FlashcardSchema);
 
 export default Flashcard;
